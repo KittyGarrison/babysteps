82fe05cce36561b2593a42066e044501fff601af
KittyGarrison <katrina.garrison@detroitlabs.com>
2016-12-04 17:26:20 -0500

    adding a todo constructor and a form that pushes a new todo to the todos array

--- scripts/controllers/toDoController.js
+++ scripts/controllers/toDoController.js
@@ -8,6 +8,24 @@
       function ToDoController() {
         var vm = this;
         vm.todoText = "";
+        vm.todos = [
+          {text : "first",
+          isComplete : false}
+        ];
+        vm.addTodo = addTodo;
+
+        function addTodo(text){
+          vm.todos.push(new Todo(text));
+          vm.todoText = "";
+        };
+
+        function Todo(text){
+          this.text = text;
+          this.isComplete = false;
+        };
+
+
+
       };
 
 })();
