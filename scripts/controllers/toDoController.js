8a209208eb2c483ccf17fa4e1af23d720fba8ec6
KittyGarrison <katrina.garrison@detroitlabs.com>
2017-01-24 20:42:25 -0500

    refactoring and removing id from view

--- scripts/controllers/toDoController.js
+++ scripts/controllers/toDoController.js
@@ -1,50 +1,53 @@
 (function() {
     'use strict';
 
-  angular
-    .module("toDoApp")
-    .controller('ListController', ListController);
+    angular
+        .module("toDoApp")
+        .controller('ListController', ListController);
 
-      function ListController() {
+    function ListController() {
         var vm = this;
-        vm.taskText = "";
-        vm.taskList = [];
-        vm.addTask = addTask;
-        vm.removeTask = removeTask;
-
-        function addTask(text){
-          vm.taskList.push(new Task(text));
-          vm.taskText = "";
-        };
-
-        function removeTask(task){
-          vm.taskList.splice(findTask(task),1)
-        };
-
-        function findTask(task){
-          return vm.taskList.indexOf(task);
-        }
 
         var count = 100;
 
-        function Task(text){
-          function incrementedId(){
-            count = count +1;
-            return count
-          };
-          this.id = incrementedId();
-          this.text = text;
-          this.isComplete = false;
-          this.editMode = false;
-          this.complete = function(){
-            this.isComplete = !this.isComplete;
-          };
-          this.edit = function(){
-            this.editMode = true;
-          };
-          this.updateText = function(){
-            this.editMode = false;
-          };
-          };
+        vm.addTask = addTask;
+        vm.removeTask = removeTask;
+        vm.taskList = [];
+        // this does not need to be here since it is instantiated with ng-model but it seems more clear to
+        vm.taskText = "";
+
+        function addTask(text) {
+            vm.taskList.push(new Task(text));
+            vm.taskText = "";
         };
+
+        function removeTask(task) {
+            vm.taskList.splice(findTask(task), 1)
+        };
+
+        function findTask(task) {
+            return vm.taskList.indexOf(task);
+        }
+
+        function incrementedId() {
+            count = count + 1;
+            return count
+        };
+
+        function Task(text) {
+            this.id = incrementedId();
+            this.text = text;
+            this.isComplete = false;
+            this.editMode = false;
+            this.complete = function() {
+                this.isComplete = !this.isComplete;
+            };
+            this.edit = function() {
+                this.editMode = true;
+            };
+            this.updateText = function() {
+                this.editMode = false;
+            };
+        };
+    };
 })();
